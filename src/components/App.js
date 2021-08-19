b9ea0424839c28d8d17c941f1d8280a8a5024fd0
m.ali bilgin <76842508+muhammedalibilgin@users.noreply.github.com>
2021-08-19 19:53:21 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -4,33 +4,30 @@ import axios from "axios";
 require("dotenv").config();
 
 // genres api get
-const genres = axios.get(
-  `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
-);
+const genres = axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
 let genresname;
 genres.then(function (x) {
-  genresname = x.data.genres.map((y) => y);
+      genresname = x.data.genres.map((y) => y);
 });
 
 class App extends React.Component {
-  state = {
-    movies: [],
-  };
+      state = {
+            movies: [],
+      };
 
-  async componentDidMount() {
-    const responsive = await axios.get(
-      `https://api.themoviedb.org/3/list/7100438?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
-    );
-    this.setState({ movies: responsive.data.items });
-  }
+      async componentDidMount() {
+            const responsive = await axios.get(`https://api.themoviedb.org/3/list/7100438?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+            this.setState({ movies: responsive.data.items });
+      }
 
-  render() {
-    return (
-      <div>
-        <Movies movieprop={this.state.movies} genresprop={genresname} />
-      </div>
-    );
-  }
+      render() {
+            return (
+                  <div>
+                        <h1>hello github</h1>
+                        <Movies movieprop={this.state.movies} genresprop={genresname} />
+                  </div>
+            );
+      }
 }
 
 export default App;
